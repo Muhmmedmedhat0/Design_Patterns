eb3645638fa0bb95f5ab125563b33597a36bde5b
mummmad_medhat <muhmmedmedhat0@gmail.com>
2026-09-17 15:45:41 +0300

    Refactor math operations to use module imports and create package.json

--- module/js/index.js
+++ module/js/index.js
@@ -1,4 +1,5 @@
-console.log('Sum', sum(1, 2));
-console.log('Subtract', subtract(1, 2));
-console.log('Divide', divide(1, 2));
-console.log('Multiply', multiply(1, 2));
+import { sum, subtract, divide, multiply } from "./math.js";
+console.log("Sum", sum(1, 2));
+console.log("Subtract", subtract(1, 2));
+console.log("Divide", divide(1, 2));
+console.log("Multiply", multiply(1, 2));
